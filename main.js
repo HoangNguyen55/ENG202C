cc13ca62eee33d1d3df416a01acd5e0b8c7ca123
HoangNguyen55 <HoangNKNguyen@proton.me>
2023-04-12 22:23:43 -0400

    a better header, toc, text block fill a page

--- main.js
+++ main.js
@@ -1,3 +1,31 @@
+// wrap every div in a wrapper div so that every div is the size of the screen
+{
+  const ele = document.querySelectorAll('body > div');
+
+  for (let i = 0, n=ele.length; i < n; i++) {
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('wrapper');
+    ele[i].parentNode.insertBefore(wrapper, ele[i]);
+    wrapper.appendChild(ele[i]);
+  }
+}
+//
+
+// TOC generator
+{
+  const ele = document.querySelectorAll('div.body');
+  const toc = document.querySelector('#dropdown-menu');
+  for (let i = 0, n=ele.length; i < n; i++) {
+    const id = ele[i].id;
+    const text = ele[i].querySelector('h2').textContent;
+    const ref = document.createElement('a')
+    ref.textContent = text;
+    ref.href = `#${id}`;
+    toc.appendChild(ref);
+  }
+}
+
 window.addEventListener('scroll', () => {
   document.documentElement.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
+  console.log(document.documentElement.style.getPropertyValue('--scroll'));
 }, false);
